4fd7a969f7df7acde27d6ab33699f9507a193b2d
titokarnavian88 <seodewamonyet88888@gmail.com>
2025-04-19 19:54:07 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline text-center mt-10">Halo Jaya Prana!</h1>
+  </React.StrictMode>,
+)
